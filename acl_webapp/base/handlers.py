acc2f292e1cc89b1c4e9327c51155d9aca029992
st4lk <myhappydo@gmail.com>
2013-11-29 17:58:51 +0400

    base handler: set username to cookie

--- acl_webapp/base/handlers.py
+++ acl_webapp/base/handlers.py
@@ -22,3 +22,13 @@ class BaseHandler(tornado.web.RequestHandler):
         # Always set _xsrf cookie
         self.xsrf_token
         self.flush()
+
+    def set_current_user(self, user):
+        if user:
+            self.set_secure_cookie('user', user)
+        else:
+            self.clear_cookie('user')
+
+    def get_current_user(self):
+        expires = self.settings.get('cookie_expires', 31)
+        return self.get_secure_cookie('user', max_age_days=expires)
