3f5a025fee02d01eadb1965dc1f523e7da10959a
st4lk <myhappydo@gmail.com>
2013-12-01 21:44:38 +0400

    test register succeeded

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -1,3 +1,4 @@
+import re
 from base import BaseTest
 
 
@@ -5,6 +6,27 @@ class RegisterTest(BaseTest):
 
     def test_page_exist(self):
         return self.page_exist('register')
+
+    def test_user_is_created(self):
+        email = 'vasya@vasya.com'
+        post_data = {
+            'email': email,
+            'password': '123123',
+            'password2': '123123',
+        }
+        reg_url = self.reverse_url('register')
+        resp = self.get(reg_url)
+        # add xsrf to post request
+        post_data['_xsrf'] = re.search(
+            '<input type="hidden" name="_xsrf" value="(.*?)"', resp.body)\
+            .group(1)
+        resp = self.post(reg_url, data=post_data)
+        # check we are redirected to home page
+        self.assertEqual(resp.code, 302)
+        self.assertEqual(resp.headers['Location'], self.reverse_url('home'))
+        # check, user is created
+        user = self.db_find_one('accounts', {'_id': email})
+        self.assertEqual(user['_id'], email)
 
 
 class LoginTest(BaseTest):
--- tests/base.py
+++ tests/base.py
@@ -1,24 +1,59 @@
 # -*- coding: utf-8 -*-
+import Cookie
 from tornado.testing import AsyncHTTPTestCase, LogTrapTestCase
+from tornado.ioloop import IOLoop
+from tornado import gen
+import motor
 from app import ACLApp
+from libs.http_test_client import TestClient
 
 
 MONGO_TEST_DB = 'acl_app_test'
 app = ACLApp(mongo_db=MONGO_TEST_DB)
 reverse_url = app.reverse_url
+db = app.settings['db']
 
 
-class BaseTest(AsyncHTTPTestCase, LogTrapTestCase):
+class BaseTest(AsyncHTTPTestCase, LogTrapTestCase, TestClient):
 
     def setUp(self):
         super(BaseTest, self).setUp()
         self.reverse_url = reverse_url
+        self.cookies = Cookie.SimpleCookie()
+        ### clear data base before each test
+        self.db_clear()
 
     def get_app(self):
         return app
 
     def page_exist(self, url_name):
         url = self.reverse_url(url_name)
-        self.http_client.fetch(self.get_url(url), self.stop)
-        response = self.wait()
-        self.assertEqual(response.code, 200)
+        resp = self.get(url)
+        self.assertEqual(resp.code, 200)
+
+    def get_new_ioloop(self):
+        return IOLoop.instance()
+
+    def db_clear(self):
+        @gen.engine
+        def async_op():
+            yield motor.Op(db.accounts.remove)
+            self.stop()
+        async_op()
+        self.wait()
+
+    def db_find_one(self, collection, data):
+        @gen.engine
+        def async_op():
+            result = yield motor.Op(db[collection].find_one, data)
+            self.stop(result)
+        async_op()
+        return self.wait()
+
+    def db_save(self, collection, data):
+        @gen.engine
+        def async_op():
+            result = yield motor.Op(db[collection].save, data)
+            self.stop(result)
+        async_op()
+        return self.wait()
