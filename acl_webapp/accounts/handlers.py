ac69158ffadcf324d632646582c3763aa5691c2b
st4lk <myhappydo@gmail.com>
2013-12-01 14:29:11 +0400

    login post ajax

--- acl_webapp/accounts/handlers.py
+++ acl_webapp/accounts/handlers.py
@@ -12,42 +12,11 @@ l = logging.getLogger(__name__)
 
 class AuthMixin(object):
 
-    def authenticate(self, user):
-        self.set_current_user(user.email)
-        self.redirect(self.reverse_url("home"))
-
-
-class RegisterHandler(BaseHandler, AuthMixin):
-    """Handler for registration page. Show and process register form.
-    """
-    def initialize(self, **kwargs):
-        super(RegisterHandler, self).initialize(**kwargs)
-        self.template_name = "accounts/register.html"
-        self.template_name_ajax = "accounts/register_ajax.html"
-
-    def get(self):
-        form = RegistrationForm()
+    def get_render_form(self, form):
         if self.is_ajax:
             self.render(self.template_name_ajax, {'form': form})
         else:
             self.render(self.template_name, {'form': form})
-
-    @gen.coroutine
-    def post(self):
-        form = RegistrationForm(self.request.arguments)
-        if form.validate():
-            usr = form.get_object()
-            usr.set_password(usr.password)
-            try:
-                yield motor.Op(usr.insert, self.db)
-            except DuplicateKeyError:
-                form.set_field_error('email', 'email_occupied')
-            else:
-                # user save succeeded
-                self.set_current_user(usr.email)
-                self.post_success()
-                return
-        self.post_failed(form)
 
     def post_success(self):
         redirect_url = self.reverse_url("home")
@@ -65,6 +34,36 @@ class RegisterHandler(BaseHandler, AuthMixin):
             self.render_json(data)
         else:
             self.render(self.template_name, {"form": form})
+
+
+class RegisterHandler(BaseHandler, AuthMixin):
+    """Handler for registration page. Show and process register form.
+    """
+    def initialize(self, **kwargs):
+        super(RegisterHandler, self).initialize(**kwargs)
+        self.template_name = "accounts/register.html"
+        self.template_name_ajax = "accounts/register_ajax.html"
+
+    def get(self):
+        form = RegistrationForm()
+        self.get_render_form(form)
+
+    @gen.coroutine
+    def post(self):
+        form = RegistrationForm(self.request.arguments)
+        if form.validate():
+            usr = form.get_object()
+            usr.set_password(usr.password)
+            try:
+                yield motor.Op(usr.insert, self.db)
+            except DuplicateKeyError:
+                form.set_field_error('email', 'email_occupied')
+            else:
+                # user save succeeded
+                self.set_current_user(usr.email)
+                self.post_success()
+                return
+        self.post_failed(form)
 
 
 class LogoutHandler(BaseHandler):
@@ -86,10 +85,7 @@ class LoginHandler(BaseHandler, AuthMixin):
 
     def get(self):
         form = LoginForm()
-        if self.is_ajax:
-            self.render(self.template_name_ajax, {'form': form})
-        else:
-            self.render(self.template_name, {'form': form})
+        self.get_render_form(form)
 
     @gen.coroutine
     def post(self):
@@ -99,10 +95,11 @@ class LoginHandler(BaseHandler, AuthMixin):
                 "email": form.email.data})
             if usr:
                 if usr.check_password(form.password.data):
-                    self.authenticate(usr)
+                    self.set_current_user(usr.email)
+                    self.post_success()
                     return
                 else:
                     form.set_field_error('password', 'wrong_password')
             else:
                 form.set_field_error('email', 'not_found')
-        self.render(self.template_name, {"form": form})
+        self.post_failed(form)
